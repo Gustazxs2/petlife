6825083ccf1166bd4baaa06fc0e4524867cdbbf2
Gustavo Brioso <gustazxscanal@gmail.com>
2026-05-26 15:07:06 -0300

    ajustando e testando 1

--- frontend/app/usuario/page.tsx
+++ frontend/app/usuario/page.tsx
@@ -56,6 +56,9 @@ async function carregarAgendamentos(email: string) {
 
   const dados = await resposta.json();
 
+console.log(dados);
+console.log(Array.isArray(dados));
+
   console.log(dados);
   console.log(email);
 
